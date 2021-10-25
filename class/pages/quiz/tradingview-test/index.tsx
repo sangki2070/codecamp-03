019c34b6910b6e580599c4b9167d96ae0004fed9
sangki2070 <devsangki@gmail.com>
2021-10-25 23:15:41 +0900

    추가 커밋

--- class/pages/quiz/tradingview-test/index.tsx
+++ class/pages/quiz/tradingview-test/index.tsx
@@ -1,48 +1,43 @@
-// <!-- TradingView Widget BEGIN -->
-// <div class="tradingview-widget-container">
-//   <div class="tradingview-widget-container__widget"></div>
-//   <div class="tradingview-widget-copyright">트레이딩뷰 제공 <a href="https://kr.tradingview.com/symbols/EURUSD/?exchange=FX" rel="noopener" target="_blank"><span class="blue-text">EURUSD환율</span></a></div>
-//   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
-//   {
-//   "symbol": "FX:EURUSD",
-//   "width": 350,
-//   "height": 220,
-//   "locale": "kr",
-//   "dateRange": "12M",
-//   "colorTheme": "dark",
-//   "trendLineColor": "rgba(41, 98, 255, 1)",
-//   "underLineColor": "rgba(41, 98, 255, 0.3)",
-//   "underLineBottomColor": "rgba(41, 98, 255, 0)",
-//   "isTransparent": false,
-//   "autosize": false,
-//   "largeChartUrl": ""
-// }
-//   </script>
-// </div>
-// <!-- TradingView Widget END -->
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
-kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-  searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      let detailAddr = !!result[0].road_address
-        ? "<div>도로명주소 : " + result[0].road_address.address_name + "</div>"
-        : "";
-      detailAddr +=
-        "<div>지번 주소 : " + result[0].address.address_name + "</div>";
+const TradingChartWrapper = styled.div`
+  padding: 15px;
+  border: 1px solid #9e9e9e;
+  border-radius: 5px;
+  height: 50vh;
+`;
 
-      let content =
-        '<div class="bAddr">' +
-        '<span class="title">법정동 주소정보</span>' +
-        detailAddr +
-        "</div>";
+export default function CoinChart() {
+  // useEffect(() => {
+  //   const tradingWrapper = document.getElementById("tradingview_f4336")
+  // })
 
-      // 마커를 클릭한 위치에 표시합니다
-      marker.setPosition(mouseEvent.latLng);
-      marker.setMap(map);
+  return (
+    <>
+      <div id="tradingview_f4336"></div>
+      <TradingChartWrapper>
+        <TradingViewWidget
+          symbol="KLAYKRW"
+          theme={Themes.DARK}
+          locale="kr"
+          allow_symbol_change={false}
+          show_popup_button={false}
+          width="200px"
+          autosize
+        />
+      </TradingChartWrapper>
+    </>
+  );
+}
 
-      // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-      infowindow.setContent(content);
-      infowindow.open(map, marker);
-    }
-  });
-});
+// "width": 980,
+// "height": 610,
+
+//  symbol="KLAYKRW"
+//           theme={Themes.DARK}
+//           locale="kr"
+//           allow_symbol_change={false}
+//           show_popup_button={false}
+//           autosize
