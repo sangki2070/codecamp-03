9398830bb33abd449b1f28ed173b9f49695ee4c0
sangki2070 <devsangki@gmail.com>
2021-10-26 13:01:21 +0900

    서버사이드렌더링 적용 예제

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function BoardPage() {
-  return <div>게시판 입니다.</div>;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="나의사이트 게시판페이지" />
+        <meta property="og:descripton" content="환영합니다!!" />
+        <meta
+          property="og:image"
+          content="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+        />
+      </Head>
+      <div>게시판 입니다.</div>
+    </>
+  );
 }
---
+++ class_build/pages/markets/[useditemId]/index.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import { gql, request } from "graphql-request";
+
+export default function MarketPage(props) {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content={props.fetchUseditem.name} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
+        <meta property="og:image" content={props.fetchUseditem.images[0]} />
+      </Head>
+      <div>마켓페이지 입니다.</div>
+    </>
+  );
+}
+
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
+export const getServerSideProps = async (context) => {
+  // 1. graphql 데이터를 요청한다.
+  const result = await request(
+    "https://backend03.codebootcamp.co.kr/graphql",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+
+  context.query.useditemId;
+
+  // 2. 요청받은 데이터를 페이지로 넘겨준다.
+
+  return {
+    props: {
+      fetchUseditem: result.fetchUseditem,
+    },
+  };
+};
