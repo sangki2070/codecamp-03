97597a391bdfbd0220c1a3ffe3cc80792a50b0a7
sangki2070 <devsangki@gmail.com>
2021-11-11 00:21:57 +0900

    ssr 테스트 4

--- freeboard_frontend/pages/products/[ProductsDetailPage]/index.tsx
+++ freeboard_frontend/pages/products/[ProductsDetailPage]/index.tsx
@@ -19,28 +19,34 @@ const ProductsReadPage = (props: any) => {
     </>
   );
 };
+export default withAuth(ProductsReadPage);
 
 const FETCH_USEDITEM = gql`
-  query fetchUsedItem($useditemId: ID!) {
-    fetchUsedItem(useditemId: $useditemId) {
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
       name
       remarks
       images
     }
   }
 `;
+
 export const getServerSideProps = async (context: any) => {
+  //* 1번째실행
+  // 페이지 안에만 적을수 있다.  getServerSideProps는 정해진 이름이라 바꾸면 안된다.
+  // 1. graphql 데이터를 요청한다.
   const result = await request(
     "https://backend03.codebootcamp.co.kr/graphql12",
     FETCH_USEDITEM,
-    { useditemId: context.query.useditemId }
+    {
+      useditemId: context.query.useditemId,
+    }
   );
-
+  console.log("123");
+  // 2. 요청받은 데이터를 페이지로 넘겨준다.
   return {
     props: {
-      fetchUseditem: result.fetchuseditem,
+      fetchUseditem: result.fetchUseditem,
     },
   };
 };
-
-export default withAuth(ProductsReadPage);
