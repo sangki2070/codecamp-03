dbb72ae129adae43cd8d5fbc50143d97d4825625
sangki2070 <devsangki@gmail.com>
2021-11-10 22:16:19 +0900

    ssr 테스트3

--- freeboard_frontend/pages/products/index.tsx
+++ freeboard_frontend/pages/products/index.tsx
@@ -1,9 +1,47 @@
+import Head from "next/head";
+import { gql, request } from "graphql-request";
+
 import { withAuth } from "../../src/components/commons/hocs/withAuth";
 
 import ProductsListContainer from "../../src/components/units/product/list/ProductsList.container";
 
-const ProductWritePage = () => {
-  return <ProductsListContainer />;
+const ProductWritePage = (props: any) => {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content={props.fetchUseditem.name} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
+        <meta
+          property="og:image"
+          content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
+        />
+      </Head>
+      <ProductsListContainer />
+    </>
+  );
+};
+
+const FETCH_USEDITEM = gql`
+  query fetchUsedItem($useditemId: ID!) {
+    fetchUsedItem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend03.codebootcamp.co.kr/graphql12",
+    FETCH_USEDITEM,
+    { useditemId: context.query.useditemId }
+  );
+
+  return {
+    props: {
+      fetchUseditem: result.fetchuseditem,
+    },
+  };
 };
 
 export default withAuth(ProductWritePage);
