ec0ec7bf6086a94d09353b4276bffbbda49db46e
sangki2070 <devsangki@gmail.com>
2021-11-13 18:41:06 +0900

    ssr 테스트 6

--- freeboard_frontend/pages/boards/[BoardsDetailPage]/index.tsx
+++ freeboard_frontend/pages/boards/[BoardsDetailPage]/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import { request } from "graphql-request";
-import { gql } from "@apollo/client";
+import { request, gql } from "graphql-request";
 
 import BoardsDetialContainerPage from "../../../src/components/units/board/read/BoardsDetailRead.container";
 
