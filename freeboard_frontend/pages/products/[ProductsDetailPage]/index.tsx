4aa140be67d0c9d17a22c799941af240a4d2ecc1
sangki2070 <devsangki@gmail.com>
2021-11-25 20:17:07 +0900

    fix ssr2

--- freeboard_frontend/pages/products/[ProductsDetailPage]/index.tsx
+++ freeboard_frontend/pages/products/[ProductsDetailPage]/index.tsx
@@ -35,24 +35,18 @@ export const getServerSideProps = async (context: any) => {
   //* 1번째실행
   // 페이지 안에만 적을수 있다.  getServerSideProps는 정해진 이름이라 바꾸면 안된다.
   // 1. graphql 데이터를 요청한다.
-  try {
-    const result = await request(
-      "https://backend03.codebootcamp.co.kr/graphql12",
-      FETCH_USEDITEM,
-      {
-        useditemId: context.query.useditemId,
-      }
-    );
-    console.log("123");
-    // 2. 요청받은 데이터를 페이지로 넘겨준다.
-    return {
-      props: {
-        fetchUseditem: result.fetchUseditem,
-      },
-    };
-  } catch (error) {
-    console.log("###################");
-    console.log(error);
-    console.log("###################");
-  }
+  const result = await request(
+    "https://backend03.codebootcamp.co.kr/graphql12",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+  console.log("123");
+  // 2. 요청받은 데이터를 페이지로 넘겨준다.
+  return {
+    props: {
+      fetchUseditem: result.fetchUseditem,
+    },
+  };
 };
